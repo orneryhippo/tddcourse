99ed9fe2a683e514caff87fa38ef9798121d02ed
orneryhippo <jesse.cassill@gmail.com>
2017-09-24 11:32:32 -0700

    Rebuilding and redeploying

--- project/__init__.py
+++ project/__init__.py
@@ -12,6 +12,25 @@ app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object('project.config.DevelopmentConfig')
 
 
+
+# instantiate the db
+db = SQLAlchemy(app)
+
+# model
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=False, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+        self.created_at = datetime.datetime.utcnow()
+
+
 @app.route("/ping", methods=['GET'])
 def ping_pong():
     return jsonify({
--- project/config.py
+++ project/config.py
@@ -7,19 +7,24 @@ class BaseConfig:
     """Base configuration"""
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class TestingConfig(BaseConfig):
     """Testing configuration"""
     DEBUG = True
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TESTING_URL')
+
 
 
 class ProductionConfig(BaseConfig):
     """Production configuration"""
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
